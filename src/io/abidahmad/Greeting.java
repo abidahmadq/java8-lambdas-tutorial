e68711a306fef4253d50705dc7d5b011ac9823ca
abid1998 <abid.programming@gmail.com>
2021-03-07 19:37:16 +0530

    Added basic lambda tutorial code

---
+++ src/io/abidahmad/Greeting.java
@@ -0,0 +1,5 @@
+package io.abidahmad;
+
+public interface Greeting {
+	public void perform();
+}
---
+++ src/io/abidahmad/HelloWorldGreeting.java
@@ -0,0 +1,11 @@
+package io.abidahmad;
+
+public class HelloWorldGreeting implements Greeting {
+
+	@Override
+	public void perform() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
